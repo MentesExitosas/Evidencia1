01ca07a8d40a2742f7480283a417259c226933b4
Andres Eduardo Delgado Galván <andreshog@gmail.com>
2020-11-04 15:03:38 -0600

    Ciclo agregado al submenÃu de listas

--- main.py
+++ main.py
@@ -33,38 +33,40 @@ while opcion >= 1 and opcion <5:
                 listaVacia.append(elementosLista)
             print("\nLISTA TERMINADA")
             print(listaVacia)
-            print('\nSelecciona una opcion:\n 1. Saber la posición de un elemento en la lista\n 2. Inserta un elemento en la lista según el índice deseado\n 3. Eliminar elemento de la lista\n 4. Ejecutar la función pop en la lista\n 5. Ordenar los elementos de la lista utilizando sort \n 6. Verificar cuantas veces se repite un valor de la lista')
-            opcionsub = int(input(' \nSeleccion SubMenu de Listas: '))
-            if opcionsub == 1:
-                print(listaVacia)
-                opcionindex = input('\n¿De cuál elemento de la lista desea saber su posicion? ')
-                print(f'La posicion del elemento es: {listaVacia.index(opcionindex)}')
-            if opcionsub == 2:
-                print(listaVacia)
-                opcioninsertnumero = int(input('\n¿En qué posicion desea agregar? '))
-                opcioninsertelemento = input('¿Cuál elemento deseas agregar? ')
-                listaVacia.insert(opcioninsertnumero, opcioninsertelemento)   
-                print(listaVacia) 
-            if opcionsub == 3:
-                print(listaVacia)
-                opcionremoveelemento = input('\nEscriba el elemento a eliminar: ')
-                listaVacia.remove(opcionremoveelemento)
-                print(listaVacia)
-            if opcionsub ==4:
-                print("\n")
-                print(listaVacia)
-                print(f"El elemento a eliminar es: {listaVacia.pop()}")
-                print(f"La lista actualizada es: {listaVacia}")
-            if opcionsub ==5:
-                print("\n")
-                print(listaVacia)
-                listaVacia.sort()
-                print(f"La lista ordenada es: {listaVacia}")
-            if opcionsub == 6:
-                print("\n")
-                print(listaVacia)
-                valorCount = input("¿Qué valor deseas saber cuantas veces se repite en la lista? \n")
-                print(f"El valor {valorCount} se repite {listaVacia.count(valorCount)} veces")
+            opcionsub = 1
+            while opcionsub >= 1 and opcionsub < 7:
+                print('\nSelecciona una opcion:\n 1. Saber la posición de un elemento en la lista\n 2. Inserta un elemento en la lista según el índice deseado\n 3. Eliminar elemento de la lista\n 4. Ejecutar la función pop en la lista\n 5. Ordenar los elementos de la lista utilizando sort \n 6. Verificar cuantas veces se repite un valor de la lista \n 7.Salir')
+                opcionsub = int(input(' \nSeleccion SubMenu de Listas: '))
+                if opcionsub == 1:
+                    print(listaVacia)
+                    opcionindex = input('\n¿De cuál elemento de la lista desea saber su posicion? ')
+                    print(f'La posicion del elemento es: {listaVacia.index(opcionindex)}')
+                if opcionsub == 2:
+                    print(listaVacia)
+                    opcioninsertnumero = int(input('\n¿En qué posicion desea agregar? '))
+                    opcioninsertelemento = input('¿Cuál elemento deseas agregar? ')
+                    listaVacia.insert(opcioninsertnumero, opcioninsertelemento)   
+                    print(listaVacia) 
+                if opcionsub == 3:
+                    print(listaVacia)
+                    opcionremoveelemento = input('\nEscriba el elemento a eliminar: ')
+                    listaVacia.remove(opcionremoveelemento)
+                    print(listaVacia)
+                if opcionsub ==4:
+                    print("\n")
+                    print(listaVacia)
+                    print(f"El elemento a eliminar es: {listaVacia.pop()}")
+                    print(f"La lista actualizada es: {listaVacia}")
+                if opcionsub ==5:
+                    print("\n")
+                    print(listaVacia)
+                    listaVacia.sort()
+                    print(f"La lista ordenada es: {listaVacia}")
+                if opcionsub == 6:
+                    print("\n")
+                    print(listaVacia)
+                    valorCount = input("¿Qué valor deseas saber cuantas veces se repite en la lista? \n")
+                    print(f"El valor {valorCount} se repite {listaVacia.count(valorCount)} veces")
             print('Se convertirá la lista a tupla: ')
             tupla = tuple(listaVacia)
             print(f' El peso de la lista {listaVacia} es: {sys.getsizeof(listaVacia)} bytes y el peso de la tupla {tupla} es: {sys.getsizeof(tupla)} bytes')
