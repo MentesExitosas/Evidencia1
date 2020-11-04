3c6cb7e613885965242bf061abc13757524218eb
Jair Arturo Torres García <cylon0223@gmail.com>
2020-11-03 22:02:28 -0600

    Se agrego append

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 import shutil
 
+listavacia = []
 print("Bienvenido al clonador de archivos")
 print("Selecciona una opción:\n 1. Ver en que directorio estoy\n 2. Clonar un archivo o directorio a otro lugar\n 3. Mover un archivo o directorio a otro lugar\n 4. Listas\n 5. Tuplas")
 opcion = int(input("Opción: "))
@@ -18,8 +19,16 @@ try:
         directorioInicial = input("Dime la ruta del directorio donde se encuentra el archivo que quiere mover:\n")
         directorioFinal = input("Dime la ruta de a donde se va a mover el archivo:\n")
         shutil.move(directorioInicial, directorioFinal)
-
-
+    elif opcion == 4:
+        print('Selecciona una opcion:\n 1.Agregar Valores a Lista\n 2.Saber posicion de un elemento en la lista\n 3.Inserta un elemento en la lista según el indicie deseado ')
+        opcionsub = int(input(' Seleccion SubMenu de Listas: '))
+        if opcionsub == 1:
+            elementoslista = int(input('Cuantos elementos desea agregar: '))
+            for elemento in range(elementoslista + 1):
+                numeroslista = int(input('Cuales numeros vas a agregar? : '))
+                listavacia.append(numeroslista)
+            print(listavacia)
+        
 except Exception:
     print(f"Ocurrió un error {sys.exc_info()[0]}")
 else:
