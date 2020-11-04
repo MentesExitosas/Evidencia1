20b78debf24c9a3f6c0a02568ddf0fe6813bded9
Andres Eduardo Delgado Galván <andreshog@gmail.com>
2020-11-04 14:28:21 -0600

    Mejora de ortografía

--- main.py
+++ main.py
@@ -17,32 +17,32 @@ while opcion >= 1 and opcion <5:
             print(f"\nEl directorio de trabajo actual es: {os.getcwd()}")
             print(f"El peso del objeto es: {sys.getsizeof(os.getcwd())} bytes")
         elif opcion == 2:
-            directorioInicial = input("\nDime la ruta del directorio donde se encuentra el archivo que quieres copiar:\n")
-            directorioFinal = input("Dime la ruta de a donde se va a copiar el archivo:\n")
+            directorioInicial = input("\nDime la ruta del directorio donde se encuentra el archivo que quieres copiar: \n")
+            directorioFinal = input("Dime la ruta de a donde se va a copiar el archivo: \n")
             shutil.copy(directorioInicial, directorioFinal)
             print(f"El peso del objeto es: {sys.getsizeof(directorioInicial)} bytes")
         elif opcion == 3:
-            directorioInicial = input("\nDime la ruta del directorio donde se encuentra el archivo que quiere mover:\n")
-            directorioFinal = input("Dime la ruta de a donde se va a mover el archivo:\n")
+            directorioInicial = input("\nDime la ruta del directorio donde se encuentra el archivo que quiere mover: \n")
+            directorioFinal = input("Dime la ruta de a donde se va a mover el archivo: \n")
             shutil.move(directorioInicial, directorioFinal)
             print(f"El peso del objeto es: {sys.getsizeof(directorioInicial)} bytes")
         elif opcion == 4:
-            elementosLista = int(input('\nCuantos elementos desea agregar: '))
+            elementosLista = int(input('\n¿Cuántos elementos desea agregar? '))
             for elemento in range(elementosLista):
-                elementosLista = (input('Cuales valores vas a agregar? : '))
+                elementosLista = (input('¿Cuáles valores vas a agregar?  '))
                 listaVacia.append(elementosLista)
             print("\nLISTA TERMINADA")
             print(listaVacia)
-            print('\nSelecciona una opcion:\n 1. Saber posición de un elemento en la lista\n 2. Inserta un elemento en la lista según el índice deseado\n 3. Eliminar elemento de la lista\n 4. Ejecutar la función pop en la lista\n 5. Ordenar los elementos de la lista utilizando sort \n 6. Verificar cuantas veces se repite un valor de la lista')
+            print('\nSelecciona una opcion:\n 1. Saber la posición de un elemento en la lista\n 2. Inserta un elemento en la lista según el índice deseado\n 3. Eliminar elemento de la lista\n 4. Ejecutar la función pop en la lista\n 5. Ordenar los elementos de la lista utilizando sort \n 6. Verificar cuantas veces se repite un valor de la lista')
             opcionsub = int(input(' \nSeleccion SubMenu de Listas: '))
             if opcionsub == 1:
                 print(listaVacia)
-                opcionindex = input('\nDe cual elemento de la lista desea saber su posicion :')
-                print(f'La posicion del elemento es : {listaVacia.index(opcionindex)}')
+                opcionindex = input('\n¿De cuál elemento de la lista desea saber su posicion? ')
+                print(f'La posicion del elemento es: {listaVacia.index(opcionindex)}')
             if opcionsub == 2:
                 print(listaVacia)
-                opcioninsertnumero = int(input('\nEn que posicion desea agregar : '))
-                opcioninsertelemento = input('Cual elemento deseas agregar : ')
+                opcioninsertnumero = int(input('\n¿En qué posicion desea agregar? '))
+                opcioninsertelemento = input('¿Cuál elemento deseas agregar? ')
                 listaVacia.insert(opcioninsertnumero, opcioninsertelemento)   
                 print(listaVacia) 
             if opcionsub == 3:
@@ -63,7 +63,7 @@ while opcion >= 1 and opcion <5:
             if opcionsub == 6:
                 print("\n")
                 print(listaVacia)
-                valorCount = input("Qué valor deseas saber cuantas veces se repite en la lista? \n")
+                valorCount = input("¿Qué valor deseas saber cuantas veces se repite en la lista? \n")
                 print(f"El valor {valorCount} se repite {listaVacia.count(valorCount)} veces")
             print('Se convertirá la lista a tupla: ')
             tupla = tuple(listaVacia)
