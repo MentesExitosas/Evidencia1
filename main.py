48833f3c56640337e8630a2c8c33d51e8fa05ebc
PatricioBrenner <patricio.brenneres@uanl.edu.mx>
2020-11-03 22:07:23 -0600

    Modify lists

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import sys
 import shutil
 
-listavacia = []
+listaVacia = []
 print("Bienvenido al clonador de archivos")
 print("Selecciona una opción:\n 1. Ver en que directorio estoy\n 2. Clonar un archivo o directorio a otro lugar\n 3. Mover un archivo o directorio a otro lugar\n 4. Listas\n 5. Tuplas")
 opcion = int(input("Opción: "))
@@ -23,13 +23,11 @@ try:
         print('Selecciona una opcion:\n 1.Agregar Valores a Lista\n 2.Saber posicion de un elemento en la lista\n 3.Inserta un elemento en la lista según el indicie deseado ')
         opcionsub = int(input(' Seleccion SubMenu de Listas: '))
         if opcionsub == 1:
-            elementoslista = int(input('Cuantos elementos desea agregar: '))
-            for elemento in range(elementoslista + 1):
-                numeroslista = int(input('Cuales numeros vas a agregar? : '))
-                listavacia.append(numeroslista)
-            print(listavacia)
+            elementosLista = int(input('Cuantos elementos desea agregar: '))
+            for elemento in range(elementosLista):
+                elementosLista = (input('Cuales numeros vas a agregar? : '))
+                listaVacia.append(elementosLista)
+            print(listaVacia)
         
 except Exception:
     print(f"Ocurrió un error {sys.exc_info()[0]}")
-else:
-    print("Valor Erróneo")
